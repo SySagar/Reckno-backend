5301cde93de0670ce08bb50408eeaec371a1ec1b
SySagar <sysagar07@gmail.com>
2022-12-25 13:17:48 +0530

    initial reckno server set-up

---
+++ public/js/main.js
@@ -0,0 +1,71 @@
+const socket = io()
+const form = document.getElementById('chat-form')
+const chatMessages = document.querySelector('.chat-messages')
+const roomName = document.getElementById('room-name')
+const usersList= document.getElementById('users')
+
+//get username and room from url
+const {username , room} = Qs.parse(location.search ,{
+    ignoreQueryPrefix : true
+})
+
+//join room
+socket.emit('joinRoom' , {username , room})
+
+
+//get room and users
+socket.on('roomUsers',({room,users})=>{
+
+    outRoomName(room)
+    outUsers(users)
+})
+
+socket.on('message',message=>{
+    console.log(message);
+    console.log("hgb")
+    outMessage(message)
+
+    //for every message scroll down
+    chatMessages.scrollTop=chatMessages.scrollHeight
+})
+
+//message submit
+form.addEventListener('submit', (e)=>{
+    e.preventDefault();
+    const msg=e.target.elements.msg.value;
+    socket.emit('chatMessage',msg);
+
+    //clear input 
+    e.target.elements.msg.value=''
+    e.target.elements.msg.focus()
+})
+
+function outMessage(message)
+{
+    const div = document.createElement('div')
+    div.classList.add('message')
+    div.innerHTML = '<p class="meta">'+message.username+' <span>'+message.time+'</span></p><p class="text">'+message.text+'</p>'
+    document.querySelector('.chat-messages').appendChild(div)
+
+    console.log(div.innerHTML);
+}
+
+//add room name to dom
+function outRoomName(room)
+{
+    roomName.innerText = room;
+}
+
+//add users to dom
+function outUsers(users)
+{
+   usersList.innerHTML=''+users.map(user => '<li>'+user.username+'</li>').join('')
+}
+
+// //return roomname
+// function getRoomName()
+// {
+//     return roomName
+// }
+
+// module.exports = getRoomName
---
+++ server.js
@@ -0,0 +1,60 @@
+const path = require('path')
+const express = require('express')
+const socket = require('socket.io')
+const formatMessage = require('./utils/messages')
+const {userJoin , getCurrentUser ,userLeave , getRoomUsers} = require('./utils/users')
+
+const app = express()
+
+//static files
+app.use(express.static('public'))
+const botname="Reckno bot"
+
+const server = app.listen(process.env.PORT || 3000 || 4000, ()=>{
+    console.log("listening to requests");
+})
+
+const io = socket(server)
+
+io.on('connection' , socket=>{
+    console.log('connection established');
+
+    socket.on('joinRoom',({username, room})=>{
+    const user  = userJoin(socket.id , username , room)
+        socket.join(user.room)
+        
+    socket.emit('message',formatMessage(botname , `Welcome to ${room}`))
+ 
+    //Broadcast
+    socket.broadcast.to(user.room).emit('message' , formatMessage(botname , user.username+" has joined the chat"))
+
+    //send users and room info
+    io.to(user.room).emit('roomUsers',{
+        room: user.room,
+        users: getRoomUsers(user.room)
+    })
+
+    })
+
+    //on disconnect
+    socket.on('disconnect',()=>{
+        //this notifies everyone
+        const user =userLeave(socket.id)
+
+        if(user)
+        io.to(user.room).emit('message',formatMessage(botname , user.username+" has left the chat"))
+
+        io.to(user.room).emit('roomUsers',{
+            room: user.room,
+            users: getRoomUsers(user.room)
+        })
+    })
+    
+    //listen for chatMessage
+    socket.on('chatMessage',(msg)=>{
+        const user = getCurrentUser(socket.id)
+        
+        io.to(user.room).emit('message',formatMessage(user.username , msg))
+    })
+
+})
